208ff000c751f0b1dbdc01e5d5277f22453509b4
Felix Mulder <felix.mulder@gmail.com>
2016-08-23 18:06:24 +0200

    Change packageview to be a div containing divs of packages
    The reason behind this change is so that the packages can be collapsed

--- js/src/html/PackageFragment.scala
+++ js/src/html/PackageFragment.scala
@@ -2,58 +2,49 @@ package dotty.tools.doc.client
 package html
 
 import scalatags.JsDom.all._
-import model.Entity
+import model._
+import model.ops._
 
 case class PackageFragment(entity: Entity) extends Fragment {
   import EntityIndex.packages
-  import model.ops._
   import util.linking._
 
   def render = view.render
 
-  val view = ul(
-    cls := "mdl-list packages",
-    {
-      val keys: Seq[String] = packages.keys.toSeq.sorted
-      val productReg = """^Product[0-9]+$""".r
-      keys.flatMap { k =>
-        val pack = packages(k)
-        val children =
-          pack.children
-            .sortBy(_.name)
-            .filterNot { ent =>
-              // Filter out ProductX where X > 3
-              productReg.findFirstIn(ent.name).map(_.slice(7, 99).toInt > 3).getOrElse(false) ||
-              // Filter out packages
-              ent.kind == "package" ||
-              // Filter out objects that have companions
-              (ent.kind == "object" && companion(ent).isDefined) ||
-              ent.name == "AnyValCompanion"
-            }
-            .map { ent =>
-              val comp = companion(ent)
-              val entUrl = linkTo(ent.path, from = entity)
-              val compUrl = comp.map(_.path).map(linkTo(_, entity)).getOrElse("#")
-              li(
-                cls := s"""mdl-list__item entity ${ if (comp.isDefined) "two" else "one" }""",
-                comp.map { _ => a(cls := "entity-button object", href := compUrl, "O") }.getOrElse(()),
-                a(
-                  cls := s"""entity-button shadowed ${ent.kind.replaceAll(" ", "")}""",
-                  href := entUrl,
-                  ent.kind(0).toUpper.toString
-                ),
-                a(
-                  cls := "entity-name",
-                  href := entUrl,
-                  ent.name
-                )
-              )
-            }
+  private[this] val productReg = """^Product[0-9]+$""".r
 
-        if (children.nonEmpty)
-          li(cls := "mdl-list__item package", href := linkTo(pack.path, entity), k) :: children.toList
-        else Nil
-      }
-    }
+  def packUl(pack: Package) = ul(
+    cls := "mdl-list packages",
+    (for {
+      ent <- pack.children
+      if !(productReg.findFirstIn(ent.name).map(_.slice(7, 99).toInt > 3).getOrElse(false) ||
+        // Filter out packages
+        ent.kind == "package" ||
+        // Filter out objects that have companions
+        (ent.kind == "object" && companion(ent).isDefined) || ent.name == "AnyValCompanion")
+    } yield {
+      val comp = companion(ent)
+      val entUrl = linkTo(ent.path, from = entity)
+      val compUrl = comp.map(_.path).map(linkTo(_, entity)).getOrElse("#")
+      li(
+        cls := s"""mdl-list__item entity ${ if (comp.isDefined) "two" else "one" }""",
+        comp.map { _ => a(cls := "entity-button object", href := compUrl, "O") }.getOrElse(()),
+        a(
+          cls := s"""entity-button shadowed ${ent.kind.replaceAll(" ", "")}""",
+          href := entUrl,
+          ent.kind(0).toUpper.toString
+        ),
+        a(cls := "entity-name", href := entUrl, ent.name)
+      )
+    }).toList
+  )
+
+  val view = div(
+    (for ((packName, pack) <- packages if pack.children.nonEmpty) yield div(
+      id := s"""${packName.replaceAll("\\.", "-")}""",
+      cls := "package-container",
+      span(cls := "mdl-layout-title", a(href := linkTo(pack.path, entity, "/index.html"), packName)),
+      packUl(pack)
+    )).toList
   )
 }
